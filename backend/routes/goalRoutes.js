9102c85ade841a6607052467aac7e72fa2a93d09
ToFJ <jehletom@hotmail.com>
2022-02-10 18:51:16 +0100

    controller,middleware setup

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,21 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { getGoals, setGoals, updateGoals, deleteGoals } = require("../controller/goalController");
 
-// dont need status
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get goals" });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Set goals" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
-});
+router.route("/").get(getGoals).post(setGoals);
+router.route("/:id").delete(updateGoals).put(deleteGoals);
 
 module.exports = router;
